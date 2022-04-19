4e362b126642c57fa2d1fd257a823d16b331f3ee
Oppo Computer <48522069+oppocomputer@users.noreply.github.com>
2022-04-19 11:50:57 +0200

    Update data-encoding.ino

--- peregrinus-main-computer/data-encoding.ino
+++ peregrinus-main-computer/data-encoding.ino
@@ -7,7 +7,7 @@ char counter = 0;
 
 
 //Object which contains the sensor readings
-struct dataPoint
+struct datapoint
 { 
   unsigned int radiation;
   long magneto_x;
@@ -21,7 +21,7 @@ struct dataPoint
   int accel_z;
 };
 
-struct finalDatapoint
+struct status
 {
   char voltage;
   char temperature;
@@ -29,42 +29,42 @@ struct finalDatapoint
 
 
 //Initialize dataPoint objects
-struct dataPoint datapoints[10]; // dataPoint array of num length
-struct dataPoint *p_dataPoint = datapoints; // "cursor" for changing data inside the dataPoint objects
+struct datapoint datapoints[10]; // dataPoint array of num length
+struct datapoint *p_datapoint = datapoints; // "cursor" for changing data inside the dataPoint objects
 
-struct finalDatapoint k;
+struct status stat;
 
 void addDatapoint(unsigned int rad_count, long mag_x, long mag_y, long mag_z, int gy_x, int gy_y, int gy_z, int acc_x, int acc_y, int acc_z, int volt, int temp)
 {
-  p_dataPoint->radiation = rad_count; // 2 + 3*4 +3*2 + 3*2 = 26 bytes w/o volt + temp
-  p_dataPoint->magneto_x = mag_x; // 26*10 = 260 bytes -> 260 + 2 + 2 = 264 bytes w/ volt + temp -> 264 byte packet size
-  p_dataPoint->magneto_y = mag_y;
-  p_dataPoint->magneto_z = mag_z;
-  p_dataPoint->gyro_x = gy_x;
-  p_dataPoint->gyro_y = gy_y;
-  p_dataPoint->gyro_z = gy_z;
-  p_dataPoint->accel_x = acc_x;
-  p_dataPoint->accel_y = acc_y;
-  p_dataPoint->accel_z = acc_z;
+  p_datapoint->radiation = rad_count; // 2 + 3*4 +3*2 + 3*2 = 26 bytes w/o volt + temp
+  p_datapoint->magneto_x = mag_x; // 26*10 = 260 bytes -> 260 + 2 + 2 = 264 bytes w/ volt + temp -> 264 byte packet size
+  p_datapoint->magneto_y = mag_y;
+  p_datapoint->magneto_z = mag_z;
+  p_datapoint->gyro_x = gy_x;
+  p_datapoint->gyro_y = gy_y;
+  p_datapoint->gyro_z = gy_z;
+  p_datapoint->accel_x = acc_x;
+  p_datapoint->accel_y = acc_y;
+  p_datapoint->accel_z = acc_z;
 
   
   //Counter logic
   if (counter == 9){
-    k.voltage = volt;
-    k.temperature = temp;
+    stat.voltage = volt;
+    stat.temperature = temp;
 
-    p_dataPoint -= 9;
+    p_datapoint -= 9;
     counter = 0; 
-    transmitData();  
+    transmitData(datapoints, stat);  
   } else 
   {
-    p_dataPoint++; //Move "cursor" to next p_dataPoint
+    p_datapoint++; //Move "cursor" to next p_dataPoint
     counter++;
   }  
 }
 
 
-void transmitData()
+void transmitData(datapoint data1[], stat data2)
 {
   
 }
